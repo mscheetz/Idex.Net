4c366a95935b1b73ddc18472f017d2a1a8311854
Matt Scheetz <mfscheetz@gmail.com>
2018-10-12 20:13:55 -0400

    Init commit

---
+++ Idex.Net/Idex.Net.Tests/IdexRepositoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Idex.Net.Tests
+{
+    public class IdexRepositoryTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Idex.Net/Idex.Net/Data/IdexRepository.cs
@@ -0,0 +1,11 @@
+﻿using Idex.Net.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idex.Net.Data
+{
+    public class IdexRepository : IIdexRepository
+    {
+    }
+}
