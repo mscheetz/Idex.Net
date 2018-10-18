3b944435c65deb1c72dc5dd609fdd8951d56c7e5
Matt Scheetz <mfscheetz@gmail.com>
2018-10-17 20:41:39 -0400

    Security helpers

---
+++ Idex.Net/Idex.Net.Tests/SecurityTests.cs
@@ -0,0 +1,28 @@
+﻿using Idex.Net.Core;
+using Idex.Net.Data;
+using Idex.Net.Data.Interface;
+using Idex.Net.Entities;
+using System;
+using Xunit;
+
+namespace Idex.Net.Tests
+{
+    public class SecurityTests : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void SecureUnSecureTest()
+        {
+            var unsecureString = "this is an unsecure string";
+
+            var secureString = Security.ToSecureString(unsecureString);
+
+            var unsecuredString = Security.ToUnsecureString(secureString);
+
+            Assert.Equal(unsecuredString, unsecureString);
+        }
+    }
+}
---
+++ Idex.Net/Idex.Net/Core/Security.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Idex.Net.Core
+{
+    public static class Security
+    {
+        /// <summary>
+        /// SecureString to string
+        /// </summary>
+        /// <param name="secureString">SecureString to convert</param>
+        /// <returns>Unsecure string</returns>
+        public static string ToUnsecureString(this SecureString secureString)
+        {
+            if (secureString == null) throw new ArgumentNullException("secureString");
+
+            var unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        /// <summary>
+        /// String to SecureString
+        /// </summary>
+        /// <param name="unsecureString">string to convert</param>
+        /// <returns>SecureString of unsecure string</returns>
+        public static SecureString ToSecureString(this string unsecureString)
+        {
+            if (unsecureString == null) throw new ArgumentNullException("unsecureString");
+
+            return unsecureString.Aggregate(new SecureString(), AppendChar, MakeReadOnly);
+        }
+
+        private static SecureString MakeReadOnly(SecureString ss)
+        {
+            ss.MakeReadOnly();
+            return ss;
+        }
+
+        private static SecureString AppendChar(SecureString ss, char c)
+        {
+            ss.AppendChar(c);
+            return ss;
+        }
+    }
+}
